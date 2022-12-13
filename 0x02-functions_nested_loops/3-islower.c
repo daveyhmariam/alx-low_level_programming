9ea032ed50a12131830006ef67abe7d205785fc0
daveyhmariam <daveyhmariam@gmail.com>
2022-12-13 15:06:59 -0800

    0-preprocessor

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/*
+ * _islower - checks case of a letter
+ * @c:  to b checked parameter
+ * return: 0 or 1
+ */
+int _islower(int c)
+{
+return(c >= 97 && c <= 122);
+return(0);
+}
