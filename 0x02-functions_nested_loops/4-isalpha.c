cecef24ebd8abe53287105dd1d56738af049972b
daveyhmariam <daveyhmariam@gmail.com>
2022-12-13 15:18:03 -0800

    0-preprocessor

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/*
+ * _isalpha - checks prameter for alphabet
+ * @c: parameter to be checked
+ *return:0 or 1
+ */
+int _isalpha(int c)
+{
+return (c = 7 && c <= 122 || c >= 65 && c <= 90 );
+return (0);
