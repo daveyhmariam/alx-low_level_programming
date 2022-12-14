eee0652f086e369e2e770ca88dac5f03cec3b1d6
daveyhmariam <daveyhmariam@gmail.com>
2022-12-14 11:26:53 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,6 +5,8 @@
  *return:0 or 1
  */
 int _isalpha(int c)
-{
-return ((c = 97 && c <= 122) || (c >= 65 && c <= 90 ));
+if ((c = 97 && c <= 122) || (c >= 65 && c <= 90 ));
+return (1);
+else
+return (0);
 }
