953ce8da7d2408cd3f14e659bca06a4398a5eaa8
daveyhmariam <daveyhmariam@gmail.com>
2023-01-16 17:10:43 +0300

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main (int argc, char* argv[])
+{
+    char* ptr=0;
+    int sum=0;
+    if (argc == 1)
+    {
+        printf("0\n");
+        return (0);
+    }
+    for (int i=1;i<argc;i++)
+    {
+        sum+=strtol(argv[i],&ptr,10);
+        if (*ptr != 0)
+        {
+            printf("Error\n");
+            return (1);
+
+        }
+                
+    }
+    printf("%d\n",sum);
+    return(0);
+}
