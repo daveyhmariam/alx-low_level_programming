9a0ff4fc2513e2578a04bd0dc218757562aa9c82
daveyhmariam <daveyhmariam@gmail.com>
2023-02-19 18:02:32 +0300

    file io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,21 @@
+#include<stdlib.h>
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    ssize_t fd,rd,wr;
+    char* buf;
+    buf=malloc(sizeof(char)*letters);
+if(!buf)
+return(0);
+fd=open(filename,O_RDONLY);
+rd=read(fd,buf,letters);
+wr=write(STDOUT_FILENO,buf,rd);
+if(fd==-1 || rd==-1 || wr==-1 || wr!=rd)
+{
+    free(buf);
+    return(0);
+}
+    free(buf);
+    return (wr);
+}
