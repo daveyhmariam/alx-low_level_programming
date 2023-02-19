fd00d4df8cc936be965e8d52b67ed56441763588
daveyhmariam <daveyhmariam@gmail.com>
2023-02-19 18:37:03 +0300

    file io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,8 +6,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
     ssize_t fd,rd,wr,len=0;
     char* buf;
     buf=malloc(sizeof(char)*letters);
-if(!buf || !filename)
-return(0);
+if(buf==NULL || filename==NULL)
+{
+    free(buf);
+    return(0);
+}
 fd=open(filename,O_RDONLY);
 rd=read(fd,buf,letters);
 wr=write(STDOUT_FILENO,buf,letters);
@@ -15,7 +18,7 @@ while(buf[len]!='\0')
 {
     len++;
 }
-if(fd==-1 || rd==-1 || wr==-1 || wr!=rd)
+if(fd==-1 || rd==-1 || wr==-1 || wr!=rd || len<letters)
 {
     free(buf);
     return(0);
