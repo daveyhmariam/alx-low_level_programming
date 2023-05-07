c8ded55ce6c35295e3e4aaede51e02f87a756951
daveyhmariam@gmail.com <daveyhmariam@gmail.com>
2023-05-07 21:11:31 +0300

    create dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -34,10 +34,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	for(i=0;i<lname;i++)
 		dog->name[i]=name[i];
-	dog->name[lname]='\0';
+	dog->name[i]='\0';
 	dog->age=age;
 	for(i=0;i<lowner;i++)
 		dog->owner[i]='\0';
-	dog->owner[lowner]='\0';
+	dog->owner[i]='\0';
 	return(dog);	
 }
