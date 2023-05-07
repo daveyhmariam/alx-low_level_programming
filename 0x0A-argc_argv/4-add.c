87520a2c5d5098131470c6232b7c72092a5933cf
daveyhmariam@gmail.com <daveyhmariam@gmail.com>
2023-05-07 17:32:45 +0300

    number of argumnts(unused arrument)

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-int main (int argc, char* argv[])
+#include <ctype.h>
+/**
+ * main - adds positive numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
 {
-    char* ptr=0;
-    int sum=0;
-    int i=0;
-    if (argc == 1)
-    {
-        printf("0\n");
-        return (0);
-    }
-    
-    for (i=1;i<argc;i++)
-    {
-        sum+=strtol(argv[i],&ptr,10);
-        if (*ptr != 0)
-        {
-            printf("Error\n");
-            return (1);
-    
-        }
-                
-    }
-    printf("%d\n",sum);
-    return(0);
+	int i, j, add = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
 }
