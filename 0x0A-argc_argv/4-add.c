439fbb20861f0e4c35e0adab1c50d7a85d145af0
daveyhmariam@gmail.com <daveyhmariam@gmail.com>
2023-05-07 17:03:50 +0300

    number of argumnts(unused arrument)

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,23 +2,25 @@
 #include <stdlib.h>
 
 
-int main (int argc, char* argv[])
+int main (int argc, char* argv[] __attribute__((__attribute_warn_unused_result__)))
 {
     char* ptr=0;
     int sum=0;
+    int i=0;
     if (argc == 1)
     {
         printf("0\n");
         return (0);
     }
-    for (int i=1;i<argc;i++)
+    
+    for (i=1;i<argc;i++)
     {
-        sum+=strtol(argv[i],&ptr,10);
+        sum+=argv[i];//strtol(argv[i],&ptr,10);
         if (*ptr != 0)
         {
             printf("Error\n");
             return (1);
-
+    
         }
                 
     }
