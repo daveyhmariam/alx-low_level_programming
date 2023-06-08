ff44df8f674a05c1b40114b21ceb715999bf473e
daveyhmariam@gmail.com <daveyhmariam@gmail.com>
2023-06-09 01:11:33 +0300

    Print wirh recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _puts_recursion: prints a string recursively
+ * _puts_recursion - prints a string recursively
  * @s: a string pointer to the string to be printed
  * Return: returns nothing
 */
 void _puts_recursion(char *s)
 {
-if(*s == '\0')
+if (*s == '\0')
 {
 _putchar('\n');
-return ;
+return;
 }
 _putchar(*s);
- _puts_recursion(s + 1);
+_puts_recursion(s + 1);
 }
