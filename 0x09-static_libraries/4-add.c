d2f8c1f7b755d2645fc6b13384e055fbfe7278a9
daveyhmariam@gmail.com <daveyhmariam@gmail.com>
2023-06-09 13:41:59 +0300

    sum up arguments

---
+++ 0x09-static_libraries/4-add.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - adds positive numbers.
+ * @argc:no of arguments
+ * @argv: aray of pointer to argument list
+ * Return: 0 - success.
+ * 1- failure
+ */
+int main(int argc, char **argv)
+{
+int i=1;
+const char *s;
+char *end;
+int sum = 0;
+if (argc <= 1)
+{
+printf("0\n");
+return (0);
+}
+for (; i < argc ; i++)
+{
+s = argv[i];
+end = argv[i];
+strtol(s, &end, 10);
+if (end != argv[i])
+{
+if (*end == '\0')
+{
+continue;
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
+}
+for(i = 1; i < argc; i++)
+{
+sum += atoi(argv[i]);
+}
+printf("%d", sum);
+return (0);
+}
