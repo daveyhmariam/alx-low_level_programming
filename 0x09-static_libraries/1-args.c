65f00273fe100f8caf1ca13f96e2fe30b549295a
daveyhmariam@gmail.com <daveyhmariam@gmail.com>
2023-06-09 10:19:50 +0300

    number of args

--- 0x09-static_libraries/1-args.c
+++ 0x09-static_libraries/1-args.c
@@ -2,6 +2,8 @@
 
 /**
  * main - prints the number of arguments passed to it
+ * @argc:no of arguments
+ * @argv: array of pointer to argument list
  * Return: number of argument passed
 */
 int main(int argc, __attribute__((unused))char **argv)
