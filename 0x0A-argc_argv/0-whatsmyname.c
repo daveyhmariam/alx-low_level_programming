4ee175282e31ae1ff4e1814b7b8cf53c9fc66d58
daveyhmariam@gmail.com <daveyhmariam@gmail.com>
2023-06-09 14:37:20 +0300

    calculate change

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: aray of pointer to argument list
  * Return: 0 on success
 */
-int main(__attribute__((unused)) int argc, char **argv)
+int main( int argc, char **argv)
 {
 printf("%s\n", argv[0]);
 return (0);
