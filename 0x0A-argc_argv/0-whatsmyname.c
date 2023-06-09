2eb008e0e22e66e1785b3b486ab1abf7ee3b5b2d
daveyhmariam@gmail.com <daveyhmariam@gmail.com>
2023-06-09 14:40:51 +0300

    program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: aray of pointer to argument list
  * Return: 0 on success
 */
-int main(__attribute__((unused)) int argc, char **argv)
+int main(int argc, char **argv)
 {
 (void)argc;
 printf("%s\n", argv[0]);
