73e54edc4c24af61b1186eca2a9268ab533d20a1
daveyhmariam@gmail.com <daveyhmariam@gmail.com>
2023-06-14 10:56:26 +0300

    concatenate strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @s1: the address of memory to print
+ * @s2: the size of the memory to print
+ *
+ * Return: pointer to the new string.
+ */
+char *str_concat(char *s1, char *s2)
+{
+int j;
+int i = 0;
+int size1, size2;
+char *ptr;
+size1 = counter(s1);
+size2 = counter(s2);
+ptr = malloc(sizeof(char) * (size1 + size2 +1));
+if (!ptr)
+return (NULL);
+for (j = 0; j < size1; j++)
+{
+if (s1[j] == '\0')
+break;
+*(ptr + j) = *(s1 + j);
+}
+for (; j < (size1 + size2); j++)
+{
+*(ptr + j) = *(s2 + i);
+i++;
+}
+return (ptr);
+}
+/**
+ * counter - counts the size of a string
+ * @str: the string arg
+ * return: the size of str
+*/
+int counter(char *str)
+{
+int i;
+int c = 0;
+if (str != NULL)
+{
+for (i = 0; str[i] != '\0'; i++)
+{
+c++;
+}
+}
+return (c);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,4 +4,6 @@
 int _putchar(char a);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int counter(char *str);
 #endif
