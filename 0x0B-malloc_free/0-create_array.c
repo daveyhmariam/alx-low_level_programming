cb3461bc39db3da3656b95611e8a24e29fc4703d
daveyhmariam@gmail.com <daveyhmariam@gmail.com>
2023-06-14 09:18:19 +0300

    create dynamic array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,18 +10,18 @@
 */
 char *create_array(unsigned int size, char c)
 {
-char* ptr;
+char *ptr;
 unsigned int i;
 if (size == 0)
 {
 return (NULL);
 }
-ptr = malloc(sizeof(char)*size);
+ptr = malloc(sizeof(char) * size);
 if (ptr == NULL)
 {
 return (NULL);
 }
-for (i=0; i < size; i++)
+for (i = 0; i < size; i++)
 {
 ptr[i] = c;
 }
