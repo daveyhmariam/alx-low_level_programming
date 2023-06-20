147cf06c4bacdb5bef4e149851681dd01dfcc3cf
daveyhmariam@gmail.com <daveyhmariam@gmail.com>
2023-06-20 04:31:59 +0300

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -23,21 +23,21 @@ for(n = 0; name[n]; n++)
 		;
 	for(o = 0 ; owner[o]; o++)
 		;
-	dog->name = malloc(n + 1);
-	dog->owner = malloc(o + 1);
-	if(!(dog->name) || !(dog->owner))
+	dog -> name = malloc(n + 1);
+	dog -> owner = malloc(o + 1);
+	if(!(dog -> name) || !(dog -> owner))
 	{
-		free(dog->name);
-		free(dog->owner);
+		free(dog -> name);
+		free(dog -> owner);
 		free(dog);
 		return (NULL);
 	}
-	for(i=0; i < n; i++)
-		dog->name[i] = name[i];
-	dog->name[i] = '\0';
-	dog->age = age;
+	for(i = 0; i < n; i++)
+		dog -> name[i] = name[i];
+	dog -> name[i] = '\0';
+	dog -> age = age;
 	for(i = 0;i < o;i++)
-		dog->owner[i] = owner[i];
-	dog->owner[i] = '\0';
-	return(dog);	
+		dog -> owner[i] = owner[i];
+	dog -> owner[i] = '\0';
+	return (dog);	
 }
