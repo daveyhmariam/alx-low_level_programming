d380c279d1f32487ec1315b7c300b88d4867e754
daveyhmariam@gmail.com <daveyhmariam@gmail.com>
2023-06-21 18:43:15 +0300

    search an element of an array

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,3 +12,14 @@
  *Return: nothing
  **/
 int int_index(int *array, int size, int (*cmp)(int))
+{
+int count;
+if (array == NULL || cmp == NULL || size <= 0)
+return (-1);
+for (count = 0; count < size; count++)
+{
+if (cmp(array[count]) != 0)
+return (count);
+}
+return (-1);
+}
