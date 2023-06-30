9aef31a3e02aeb2bf0bce94dc0a06555eff8ddac
daveyhmariam@gmail.com <daveyhmariam@gmail.com>
2023-06-30 23:12:48 +0300

    list_len

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_node - add a new node at the beginning of a linked list
+ * @head: double pointer to the list_t list
+ * @str: string to add in the node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	unsigned int length= 0;
+
+	while (str[length])
+		length++;
+
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = length;
+	new_node->next = (*head);
+	(*head) = new_node;
+
+	return (*head);
+}
