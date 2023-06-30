1fca41c62b7c2b0686e6a506ceb5a2af3db1e3dc
daveyhmariam@gmail.com <daveyhmariam@gmail.com>
2023-06-30 22:24:56 +0300

    print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "list.h"
+
+
+
+/// @brief print size f list
+/// @param h linked list
+/// @return 
+size_t print_list(const list_t *h){
+    size_t size = 0;
+    if(h == NULL){
+        return 0;
+    }
+    while(h!= NULL){
+        if(h->str != NULL){
+            printf("[%u] %s\n" , h->len , h->str);
+        }
+        else
+           printf("[0] (nil)\n");
+           h = h->next;
+           size++;
+    }
+    return size;
+
+}
