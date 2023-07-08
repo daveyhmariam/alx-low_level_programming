0a5a03ed1fc340e0f423624db6c1bdd8c6c6158e
daveyhmariam@gmail.com <daveyhmariam@gmail.com>
2023-07-08 04:28:22 +0300

    recursion

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc:no of arguments
+ * @argv: aray of pointer to argument list
+ * Return: 0 on success
+*/
+int main(int argc, char **argv)
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
