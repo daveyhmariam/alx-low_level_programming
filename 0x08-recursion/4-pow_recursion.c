79323439b2a4681513fed96b66cba8a742b33417
daveyhmariam <daveyhmariam@gmail.com>
2023-08-31 00:31:57 +0300

    exponential with recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *factorial -  function that returns the value of x raised to the power of y.
+ *_pow_recursion -  function that returns the value of x raised to the power of y.
  * @x: base
  * @y: exponent
  * Return: int
