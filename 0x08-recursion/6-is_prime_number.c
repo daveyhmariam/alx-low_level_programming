b586b892c5e1058ad2dedaeb7d010413285ef938
daveyhmariam <daveyhmariam@gmail.com>
2023-08-31 01:12:56 +0300

    prime number with recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,10 +2,27 @@
 #include "main.h"
 /**
  *s_prime_number -  function that tells prime number
- * @y: exponent
+ * @n: int argument
  * Return: int
 */
 int is_prime_number(int n)
 {
     
+    if (n <= 1)
+        return (0);
+    return (prime(n, n-1));
+}
+/**
+ * prime - checks prime number
+ * @n: the number to be checked
+ * @y: divisor
+ * Return: 1 for prime number 0 if not
+*/
+int prime(int n, int y)
+{
+if (y == 1)
+    return (1);
+if (n % y == 0 && y > 0)
+    return (0);
+return (prime(n, y - 1));   
 }
