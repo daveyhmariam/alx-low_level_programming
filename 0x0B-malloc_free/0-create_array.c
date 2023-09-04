9ddf5dc04c3a12b629c9d8f03e129c0266f1d143
daveyhmariam <daveyhmariam@gmail.com>
2023-09-04 21:00:49 +0300

    creates an array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,17 +9,17 @@
 */
 char *create_array(unsigned int size, char c)
 {
-    char *arr;
-    unsigned int i = 0;
-    if (size == 0)
-        return (NULL);
-    arr = (char *) malloc(sizeof(char) * size);
-    if (!arr)
-        return(NULL);
-    size -= 1;
-    while (i++ < size)
-    {
-    arr[i] = c;
-    }
-    return (arr);
+char *arr;
+unsigned int i = 0;
+if (size == 0)
+return (NULL);
+arr = (char *) malloc(sizeof(char) * size);
+if (!arr)
+return (NULL);
+size -= 1;
+while (i++ < size)
+{
+arr[i] = c;
+}
+return (arr);
 }
