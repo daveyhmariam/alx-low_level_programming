cd3971722d00d3a0ba5d74dcba2ca3ab00cffc0d
daveyhmariam <daveyhmariam@gmail.com>
2023-09-04 20:51:14 +0300

    creates an array of chars

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * create_array - function that creates an array of chars,
+ * and initializes it with a specific char
+ * @size: size of the char
+ * @char: the char to be written
+ * Return: NULL if size = 0
+ *         pointer to the array, or NULL if it fails
+*/
+char *create_array(unsigned int size, char c)
+{
+    char *arr;
+    unsigned int i = 0;
+    if (size == 0)
+        return (NULL);
+    arr = (char *) malloc(sizeof(char) * size);
+    if (!arr)
+        return(NULL);
+    size -= 1;
+    while (i++ < size)
+    {
+    arr[i] = c;
+    }
+    return (arr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,12 @@
+What is the difference between automatic and dynamic allocation
+What is malloc and free and how to use them
+Why and when use malloc
+How to use valgrind to check for memory leak
+
+0. Float like a butterfly, sting like a bee
+
+Write a function that creates an array of chars, and initializes it with a specific char.
+
+Prototype: char *create_array(unsigned int size, char c);
+Returns NULL if size = 0
+Returns a pointer to the array, or NULL if it fails
