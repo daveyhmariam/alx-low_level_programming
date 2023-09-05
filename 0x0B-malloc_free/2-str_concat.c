acca54c2666dd568a540af16df2fc81abe889850
daveyhmariam <daveyhmariam@gmail.com>
2023-09-05 21:50:21 +0300

    dynamic string concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * str_concat - a function that concatenates two strings.
+ * @s1: pointer to string 1
+ * @s2: pointer to string 2
+ * Return: NULL on failure
+ *         Pointer to the concateated string
+*/
+char *str_concat(char *s1, char *s2)
+{
+int len;
+int i,c;
+char *arr;
+i = 0;
+c = 0;
+len = _strlen(s1) + _strlen(s2) - 1;
+arr = (char *) malloc (sizeof(char) * len);
+if (!arr)
+return (NULL);
+while (i < len && s1[i] != '\0')
+{
+arr[i] = s1[i];
+i++;
+}
+while (i < len && s2[c] != '\0')
+{
+arr[i] = s2[c];
+i++;
+c++;
+}
+arr[i] = '\0';
+return (arr);
+}
+/**
+ * _strlen - a function that counts length of a string
+ * @str: string length
+ * Return: length of the string
+*/
+int _strlen(char *str)
+{
+int count = 0;
+if (!str && *str == '\0')
+return (0);
+while (str[count] != '\0')
+{
+count++;
+}
+count++;
+return (count);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,4 +6,5 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 int _strlen(char *str);
 void simple_print_buffer(char *buffer, unsigned int size);
+char *str_concat(char *s1, char *s2);
 #endif
