119c0f4b6c23c34111d4a29686d68ec7a27e6db7
daveyhmariam <daveyhmariam@gmail.com>
2023-09-14 03:28:53 +0300

    index iterator

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,9 +10,10 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
-for(i = 0; i < size; i++)
+if (size <= 0; || cmp == NULL || array == NULL)
+for (i = 0; i < size; i++)
 {
-if(cmp(array[i]))
+if (cmp(array[i]))
 return (i);
 }
 return (i);
