f4e6ba4f37be19fd5b3179333559a943b26e7f39
daveyhmariam <daveyhmariam@gmail.com>
2023-09-14 03:24:32 +0300

    indx iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,10 +2,16 @@
 /**
  * array_iterator - executes a function over an array
  * @array: an array argument
+ * @size: size of the array
  * @action: a function pointer that takes integer and returns nothing
  * Return: nothing
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    
+size_t i;
+if (array == NULL || action == NULL || size <= 0)
+return;
+for(i = 0; i < size; i++)
+action(array[i]);
+return;
 }
