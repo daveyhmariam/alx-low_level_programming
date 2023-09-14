303b9b48767cc38a6f23fe4bdc4b98af90624482
daveyhmariam <daveyhmariam@gmail.com>
2023-09-14 03:47:03 +0300

    index iterator

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,22 +1,25 @@
+#include <stdio.h>
 #include "function_pointers.h"
+
 /**
- * int_index -  function that searches for an integer
- * @array: array of integers
- * @size: size of the array
- * @cmp: a pointer to a function that accepts int and returns int
- * Return: on success - index of the first matched integer
- *         -1 if index is less than zero or no match is found
-*/
+ *array_iterator - a function that effects
+ *a function parameter on elements of array
+ *@array: a array to be operand
+ *@size: size of an array
+ *@action: a function pointer pointing to
+ *operating function accepting int and returning void
+ *Return: nothing
+ **/
 int int_index(int *array, int size, int (*cmp)(int))
 {
-int i, check;
-if (size <= 0 || cmp == NULL || array == NULL)
+int count, check;
+if (array == NULL || cmp == NULL || size <= 0)
 return (-1);
-for (i = 0; i < size; i++)
+for (count = 0; count < size; count++)
 {
-check = cmp(array[i]);
+check = cmp(array[count]);
 if (check != 0)
-return (i);
+return (count);
 }
 return (-1);
 }
