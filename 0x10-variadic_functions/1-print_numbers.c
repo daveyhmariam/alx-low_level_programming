2b04021ce0203fee62f7ba3bf8682ce61aea482d
daveyhmariam <daveyhmariam@gmail.com>
2023-09-15 03:46:16 +0300

    sum args

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - a function that prints all integer args with separator
+ * @separator: a separator sign
+ * @n: number of args
+ * Return: void
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list l;
+unsigned int i;
+va_start(l, n);
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(l, int));
+if (separator != NULL)
+printf("%s", separator);
+}
+va_end(l);
+printf("\n");
+return;
+}
