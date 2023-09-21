735633df915ba855aa420326d55a7c3dd8027f61
Danielyilma <yilmad752@gmail.com>
2023-09-21 17:46:51 +0300

    singly linked list

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * myStartupFun - excuted before main function
+ *
+ */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\nI ");
+	printf("bore my house upon my back!\n");
+}
