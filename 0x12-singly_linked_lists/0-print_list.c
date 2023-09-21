ab3a88242141ef7c6be09c6fc34d1981c46df92d
daveyhmariam <daveyhmariam@gmail.com>
2023-09-21 18:06:03 +0300

    linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,6 +10,20 @@
 
 size_t print_list(const list_t *h)
 {
-	
+	unsigned int length = 0;
+
+	if (h == NULL)
+	return (0);
+
+	while (h != NULL)
+	{
+
+		if (!h->str)
+		printf("[0] (nil)\n");
+		else
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		length++;
+	}
 	return (length);
 }
