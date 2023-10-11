f79039db5656eefcebdd4775ccce5be8039c7d86
daveyhmariam <daveyhmariam@gmail.com>
2023-10-11 10:12:59 +0300

    creat file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,10 +17,6 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 	return (-1);
 
-	if (text_content == NULL)
-	return (1);
-
-	
 
 	while (text_content[i] != '\0')
 	i++;
