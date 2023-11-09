65ea71c8c708f174fef2d466fe79b425c2a95544
daveyhmariam <daveyhmariam@gmail.com>
2023-11-09 22:51:48 +0300

    free list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -9,14 +9,13 @@
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp;
-
-	if (head != NULL)
-		while (head->prev != NULL)
+	if (head)
+		while (head->prev)
 			head = head->prev;
-
 	while ((tmp = head) != NULL)
 	{
 		head = head->next;
 		free(tmp);
 	}
+
 }
