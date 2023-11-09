0f95e9d55de02037cb603099304fff07609a96af
daveyhmariam <daveyhmariam@gmail.com>
2023-11-09 21:35:49 +0300

    get node

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -15,6 +15,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	if (!head)
 		return (NULL);
+	while (head->prev)
+		head = head->prev;
 	count = 0;
 	tmp = head;
 	for (; count <= index && tmp; count++)
