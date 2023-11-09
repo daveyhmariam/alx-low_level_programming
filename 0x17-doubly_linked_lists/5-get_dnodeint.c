5631d4db61ab0c116249c939cce69b853ce919bd
daveyhmariam <daveyhmariam@gmail.com>
2023-11-09 21:34:33 +0300

    get node

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -4,29 +4,29 @@
  * get_dnodeint_at_index - return specified node
  * @head: entry point to linked list
  * @index: specified index of node
- * @return: poiter to a node
+ * Return: pointer to a node
 */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int count;
-    dlistint_t *tmp;
+	unsigned int count;
+	dlistint_t *tmp;
 
 
-    if (!head)
-        return (NULL);
-    count = 0;
-    tmp = head;
-    for (; count <= index && tmp; count++)
-    {
-        if (tmp->next != NULL)
-            {
-                tmp = tmp->next;
-            }
-        else
-            {
-                return (NULL);
-            }
-    }
-    return (tmp->prev);
+	if (!head)
+		return (NULL);
+	count = 0;
+	tmp = head;
+	for (; count <= index && tmp; count++)
+	{
+		if (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+	return (tmp->prev);
 }
