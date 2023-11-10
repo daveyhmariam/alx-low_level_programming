e33db7162f171f99e9c04de32db051a8aa0c6be4
daveyhmariam <daveyhmariam@gmail.com>
2023-11-10 10:26:44 +0300

    free list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,16 +25,18 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new->next = NULL;
 		new->prev = NULL;
+		*h = new;
 		return (NULL);
 	}
-	if (idx ==0)
+	if (idx == 0)
 	{
 		new->next = tmp;
 		new->prev = NULL;
+		tmp->prev = new;
 		return (new);
 	}
-	
-	while(++count != idx)
+
+	while (++count != idx)
 	{
 		if (tmp->next)
 			tmp = tmp->next;
