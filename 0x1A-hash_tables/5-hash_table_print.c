ce167a39aee9c74e76dd9afec8d53399922ac9a9
daveyhmariam <daveyhmariam@gmail.com>
2023-11-30 23:11:39 +0300

    update 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -19,17 +19,23 @@ void hash_table_print(const hash_table_t *ht)
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		if (comma != 0)
-			printf(", ");
+
+
 		tmp = ht->array[i];
 		if (tmp)
-			while (tmp)
+		{
+			if (comma != 0)
+				printf(", ");
+			while (tmp != NULL)
 			{
 				printf("'%s': '%s'", tmp->key, tmp->value);
 				tmp = tmp->next;
 				if (tmp)
 					printf(", ");
 			}
+			comma = 1;
+		}
+
 	}
 	printf("}\n");
 }
