717fc1104ad4edc56ea53ee49dc71c44d243c024
daveyhmariam <daveyhmariam@gmail.com>
2023-11-30 20:57:09 +0300

    update 6-hash_table_delete.c

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - free hash table
+ * @ht: hash table entry
+ * Return: Nothing
+*/
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *tmp, *tmp1;
+
+	if (!ht)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+			{
+				tmp = ht->array[i];
+				while (tmp)
+				{
+					tmp1 = tmp;
+					tmp = tmp->next;
+					free(tmp1->key);
+					free(tmp1->value);
+					free(tmp1);
+				}
+			}
+	}
+	free(ht->array);
+	free(ht);
+}
