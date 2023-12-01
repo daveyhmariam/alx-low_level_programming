e1deb50aa65e7053d2612de7342515a04f79f71f
daveyhmariam <daveyhmariam@gmail.com>
2023-12-01 17:14:21 +0300

    update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,28 +17,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 	idx = key_index((const unsigned char *)key, ht->size);
-	if (idx >= ht->size)
-		return (0);
+
 	hash_node = (hash_node_t *) malloc(sizeof(hash_node_t));
 	if (hash_node == NULL)
 		return (0);
-	hash_node->key = (char *) malloc(sizeof(key));
-	if (hash_node->key == NULL)
-	{
-		return (0);
-	}
-	hash_node->value = (char *) malloc(sizeof(value));
-	if (hash_node->value == NULL)
-	{
-		free(hash_node->key);
-		return (0);
-	}
+
 	hash_node->key = strdup(key);
-	if (hash_node->key == NULL)
-		return (0);
+
 	hash_node->value = strdup(value);
-	if (hash_node->value == NULL)
-		return (0);
+
 	hash_node->next = ht->array[idx];
 	ht->array[idx] = hash_node;
 	return (1);
