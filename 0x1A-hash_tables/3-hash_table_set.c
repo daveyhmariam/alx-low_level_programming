365c7663f3427153b9f6ef41e31dda0aedbccbec
daveyhmariam <daveyhmariam@gmail.com>
2023-12-01 16:29:17 +0300

    update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,40 +12,33 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *hash_node;
-	int idx;
-	hash_node_t *tmp;
+	unsigned long int idx;
 
 	if (ht == NULL)
 		return (0);
 	if (key == NULL)
 		return (0);
-	if (key_index((const unsigned char *)key, ht->size) >= ht->size)
+	idx = key_index((const unsigned char *)key, ht->size);
+	if (idx >= ht->size)
 		return (0);
 	hash_node = (hash_node_t *) malloc(sizeof(hash_node_t));
 	if (hash_node == NULL)
 		return (0);
 	hash_node->key = (char *) malloc(sizeof(key));
-	hash_node->value = (char *) malloc(sizeof(value));
-	if (!hash_node->key || !hash_node->value)
+	if (hash_node->key == NULL)
+	{
 		return (0);
+	}
+	hash_node->value = (char *) malloc(sizeof(value));
+	if (hash_node->value == NULL)
+	{
+		free(hash_node->key);
+		return (0);
+	}
 	hash_node->key = strdup(key);
 	hash_node->value = strdup(value);
-	hash_node->next = NULL;
 
-	idx = key_index((const unsigned char *)key, ht->size);
-	tmp = ht->array[idx];
-	if (ht->array[idx] == NULL)
-	{
-		ht->array[idx] = hash_node;
-	}
-
-	else if (ht->array[idx] != NULL)
-	{
-		while (tmp && tmp->next)
-		{
-			tmp = tmp->next;
-		}
-		tmp->next = hash_node;
-	}
+	hash_node->next = ht->array[idx];
+	ht->array[idx] = hash_node;
 	return (1);
 }
