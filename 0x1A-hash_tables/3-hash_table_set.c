a3c49b09a618af26485e2c8ba6acb7565abeebd1
daveyhmariam <daveyhmariam@gmail.com>
2023-12-01 17:07:57 +0300

    update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,9 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *hash_node;
 	unsigned long int idx;
 
-	if (ht == NULL)
-		return (0);
-	if (key == NULL)
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
 	idx = key_index((const unsigned char *)key, ht->size);
 	if (idx >= ht->size)
