e54c90f97df03aa485c08d05f338a6f780f2b3b7
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-03-13 21:48:04 +0300

    interpolation search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,4 +8,6 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int cal_pos(int *array, size_t high, size_t low, int value);
 #endif
