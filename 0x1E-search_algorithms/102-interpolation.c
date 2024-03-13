eb379fa594add15adb1317f4af9f6ce82f4b291d
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-03-13 22:03:20 +0300

    interpolation search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,52 +1,54 @@
 #include "search_algos.h"
 
+
 /**
- * interpolation_search - searches for a value in an array of
- * integers using the Interpolation search algorithm
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
+ * interpolation_search - searches for a value in a sorted array of integers
+ *                          using the Interpolation search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to check
+ * Return: the index where value is located
+ *          -1 if array is NULL or no match is found
  */
+
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t pos, low, high;
-	double f;
+	size_t high = size - 1, low = 0;
+	int pos = 0;
 
-	if (array == NULL)
-		return (-1);
-
-	low = 0;
-	high = size - 1;
-
-	while (size)
+	while (pos >= 0 && pos < (int) size)
 	{
-		f = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
-		pos = (size_t)(low + f);
-		printf("Value checked array[%d]", (int)pos);
+		pos = cal_pos(array, high, low, value);
 
-		if (pos >= size)
-		{
-			printf(" is out of range\n");
-			break;
-		}
-		else
-		{
-			printf(" = [%d]\n", array[pos]);
-		}
-
+		if (pos < (int) size)
+			printf("Value checked array[%d] = [%d]\n", (int)pos, array[pos]);
 		if (array[pos] == value)
-			return ((int)pos);
-
+			return (pos);
 		if (array[pos] < value)
 			low = pos + 1;
-		else
+		if (array[pos] > value)
 			high = pos - 1;
-
 		if (low == high)
 			break;
 	}
-
+	if (pos >= (int) size)
+	{
+		printf("Value checked array[%d] is out of range\n", (int)pos);
+	}
 	return (-1);
+}
+
+/**
+ * cal_pos - calculate prob position for interpolation search
+ * @array: pointer to the first element of the array to search in
+ * @high: upper index
+ * @low: lower index
+ * Return: the prob position
+ **/
+int cal_pos(int *array, size_t high, size_t low, int value)
+{
+	size_t pos = ((high - low) / (array[high] - array[low]))
+				* (value - array[low]);
+	pos += low;
+	return ((int) pos);
 }
