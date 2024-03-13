51a56343dc8cac7eff36175d5386e27e39bcbdb1
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-03-13 15:51:06 +0300

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -27,7 +27,7 @@ int jump_search(int *array, size_t size, int value)
 			break;
 	}
 	printf("Value found between indexes [%d] and [%d]\n", l, h);
-	while (array[l] <= value && l <= (h < (int)(size - 1) ? h : (int)(size - 1)))
+	while (array[l] < value && l < (h < (int)(size - 1) ? h : (int)(size - 1)))
 	{
 		printf("Value checked array[%d] = [%d]\n", l, array[l]);
 		if (array[l] == value)
