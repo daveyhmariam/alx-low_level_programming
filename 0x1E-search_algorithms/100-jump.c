b8853344a77d6f380ac4dddba21728fdbc57b2b8
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-03-13 19:38:01 +0300

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -9,28 +9,68 @@
  * Return: the index where value is located
  *          -1 if array is NULL or no match is found
  */
+#include "search_algos.h"
+
+int linear(int *array, int left, int right, int value);
+
+/**
+ * jump_search - searches a value one by one in the array
+ *
+ * @array: the array to be searched from
+ * @size: the size of the array
+ * @value: the value needed
+ *
+ * Return: -1 if it is not found else index of the value
+*/
+
 int jump_search(int *array, size_t size, int value)
 {
-	int l, h, rt;
+	int left = 0, right, val_index = -1, jump = sqrt(size);
 
-	if (array == NULL)
-		return (-1);
-	rt = (int) sqrt((double)size);
-	l = 0;
-	h = 0;
-	while (h < (int) size && array[h] < value)
-	{
-		printf("Value checked array[%d] = [%d]\n", h, array[h]);
-		l = h;
-		h += rt;
-	}
-	printf("Value found between indexes [%d] and [%d]\n", l, h);
-	while (array[l] <= value && l <= (h < (int)(size - 1) ? h : (int)(size - 1)))
-	{
-		printf("Value checked array[%d] = [%d]\n", l, array[l]);
-		if (array[l] == value)
-			return (l);
-		l = l + 1;
-	}
+    right = jump;
+
+	if (!array)
 	return (-1);
+
+    for (; left < (int) size; right += jump, left += jump)
+    {
+        if (right < (int) size)
+        {
+            if (array[right] == value)
+            {
+                printf("Value found between indexes [%d] and [%d]\n", left, right);
+                val_index = right;
+                break;
+            }
+            else if (array[right] > value)
+            {
+                printf("Value found between indexes [%d] and [%d]\n", left, right);
+                val_index = linear(array, left, right, value);
+                break;
+            }
+        }
+        else
+        {
+            printf("Value found between indexes [%d] and [%d]\n", left, right);
+            val_index = linear(array, left, (int)size, value);
+        }
+        printf("Value checked array[%d] = [%d]\n", left, array[left]);
+        printf("Value checked array[%d] = [%d]\n", right, array[right]);
+    }
+
+	return (val_index);
+}
+
+
+int linear(int *array, int left, int right, int value)
+{
+    for (; left < right; left++)
+    {
+        printf("Value checked array[%d] = [%d]\n", left, array[left]);
+        if (array[left] == value)
+        {
+            return (left);
+        }
+    }
+    return (-1);
 }
