827f45fa25152c0123585780730b4f3c17370e57
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-03-13 19:33:16 +0300

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,7 +13,7 @@ int jump_search(int *array, size_t size, int value)
 {
 	int l, h, rt;
 
-	if (array == NULL || size == 0)
+	if (array == NULL)
 		return (-1);
 	rt = (int) sqrt((double)size);
 	l = 0;
