11578c31afbc578d079550aca7ce8696186b6e4e
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-03-14 09:55:35 +0300

    jump search non exsisting value

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,36 +1,44 @@
 #include "search_algos.h"
+#include <math.h>
 
 /**
- * jump_search - searches for a value in a sorted
- *              array of integers using the Jump search algorithm
- * @array: pointer to the first element of the array to search in
- * @size: number of elements in array
- * @value: value to check
- * Return: the index where value is located
- *          -1 if array is NULL or no match is found
+ * jump_search - searches for a value in an array of
+ * integers using the Jump search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
 int jump_search(int *array, size_t size, int value)
 {
-	int l, h, rt;
+	int index, m, k, prev;
 
-	if (array == NULL || size == 0 || array[size - 1] < value)
+	if (array == NULL || size == 0)
 		return (-1);
-	rt = (int) sqrt((double)size);
-	l = 0;
-	h = 0;
-	while (h < (int) size && array[h] < value)
+
+	m = (int)sqrt((double)size);
+	k = 0;
+	prev = index = 0;
+
+	do {
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+
+		if (array[index] == value)
+			return (index);
+		k++;
+		prev = index;
+		index = k * m;
+	} while (index < (int)size && array[index] < value);
+
+	printf("Value found between indexes [%d] and [%d]\n", prev, index);
+
+	for (; prev <= index && prev < (int)size; prev++)
 	{
-		printf("Value checked array[%d] = [%d]\n", h, array[h]);
-		l = h;
-		h += rt;
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		if (array[prev] == value)
+			return (prev);
 	}
-	printf("Value found between indexes [%d] and [%d]\n", l, h);
-	while (array[l] <= value && l <= (h < (int)(size - 1) ? h : (int)(size - 1)))
-	{
-		printf("Value checked array[%d] = [%d]\n", l, array[l]);
-		if (array[l] == value)
-			return (l);
-		l = l + 1;
-	}
+
 	return (-1);
 }
