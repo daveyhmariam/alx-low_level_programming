9e55fc30136093d3fe542ec6d67fe33a1d06f290
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-03-16 23:27:40 +0300

    advanced binary recursion

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,4 +11,5 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+int split_array(int *array, int high, int low, int value);
 #endif
