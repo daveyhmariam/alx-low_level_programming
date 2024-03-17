5614eb96097d98f7336f950a0c97cbaa251c443a
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-03-17 14:27:22 +0300

    style

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -2,7 +2,7 @@
 
 
 /**
- * binary_search - searches for a value in a sorted array of
+ * advanced_binary - searches for a value in a sorted array of
  *                  integers using the Binary search algorithm
  * @array: pointer to the first element of the array to search in
  * @size: number of elements in array
@@ -19,20 +19,26 @@ int advanced_binary(int *array, size_t size, int value)
 		return (-1);
 	low = 0;
 	high = (int) size - 1;
-	return (split_array(array, high, low, value));	
+	return (split_array(array, high, low, value));
 }
 
+
+/**
+ * split_array - perform binary search
+ * @array: pointer to the first element of the array to search in
+ * @high: higher index
+ * @low: lower index
+ * @value: value to find
+ * Return: the index where value is located
+ *          -1 if array is NULL or no match is found
+ */
 int split_array(int *array, int high, int low, int value)
 {
 	int mid, i;
 
 	printf("Searching in array:");
-	i = low;
-	while (i <= high)
-	{
-		printf("%s%d", i == low ? " ": ", ", array[i]);
-		i++;
-	}
+	for (i = low; i <= high; i++)
+		printf("%s%d", i == low ? " " : ", ", array[i]);
 	printf("\n");
 	mid = (high + low) / 2;
 	if (low <= high)
