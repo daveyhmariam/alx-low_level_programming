60f8f081931807e815998383eb65cbff9c0fd08f
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-03-17 14:12:13 +0300

    null array

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -13,29 +13,36 @@
 
 int advanced_binary(int *array, size_t size, int value)
 {
-	int low, high, mid, i;
+	int low, high;
 
 	if (array == NULL)
 		return (-1);
-
 	low = 0;
-	high = (int) (size - 1);
+	high = (int) size - 1;
+	return (split_array(array, high, low, value));	
+}
 
-	while (low < high)
+int split_array(int *array, int high, int low, int value)
+{
+	int mid, i;
+
+	printf("Searching in array:");
+	for (i = low; i <= high; i++)
+		printf("%s%d", i == low ? " ": ", ", array[i]);
+	printf("\n");
+	mid = (high + low) / 2;
+	if (low <= high)
 	{
-		mid = (high + low) / 2;
-		printf("Searching in array");
-		for (i = low; i <= high; i++)
-			printf("%s%d", (i == low) ? ": " : ", ", array[i]);
-		printf("\n");
+		if (array[mid - 1] == value)
+			return (split_array(array, mid, mid - 1, value));
 		if (array[mid] == value && array[mid - 1] != value)
 			return (mid);
-        if (array[mid - 1] == value)
-            high = mid;
+		if (array[mid] < value && mid < high)
+			return (split_array(array, high, mid + 1, value));
 		if (array[mid] > value)
-			high = mid - 1;
-		else
-			low = mid + 1;
+			return (split_array(array, mid - 1, low, value));
+		if (low == high)
+			return (-1);
 	}
 	return (-1);
 }
